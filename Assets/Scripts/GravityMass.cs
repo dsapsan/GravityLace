f66285086dea0191694c2813fc5cf70d255fd10d
Dmitry S <dsokolov@vizex.ru>
2020-09-16 20:20:43 +0300

    Simple gravity model

---
+++ Assets/Scripts/GravityMass.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace GravityLace
+{
+    public class GravityMass : MonoBehaviour
+    {
+        [SerializeField]
+        private double CurrentMass = default;
+        [SerializeField]
+        private Vector3 CurrentVelocity = default;
+
+        public double Mass => CurrentMass;
+
+        //To do: add double precision
+        public Vector3 Position { get; set; }
+        public Vector3 Velocity { get; set; }
+
+        private void Awake()
+        {
+            Position = transform.position / (float)GravitySystem.ScaleFactor;
+            Velocity = CurrentVelocity;
+            GravitySystem.Register(this);
+        }
+
+        private void Update()
+        {
+            CurrentVelocity = Velocity;
+            Velocity = CurrentVelocity;
+            transform.position = Position * (float) GravitySystem.ScaleFactor;
+        }
+
+        private void OnDestroy()
+        {
+            GravitySystem.Unregister(this);
+        }
+    }
+}
---
+++ Assets/Scripts/GravitySystem.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace GravityLace
+{
+    public class GravitySystem : MonoBehaviour
+    {
+        private static double GravitationalConstant = 6.674e-11;
+        private static double TimeSpeed = 86400;
+
+        private List<GravityMass> Masses = new List<GravityMass>();
+
+        //To do: create physics helper class with constants
+        public static double ScaleFactor { get; } = 10e-7;
+
+        public static GravitySystem Instance { get; private set; }
+
+        public static void Register(GravityMass mass)
+        {
+            CheckInstance();
+            Instance.Masses.Add(mass);
+        }
+
+        public static void Unregister(GravityMass mass)
+        {
+            CheckInstance();
+            Instance.Masses.Remove(mass);
+        }
+
+        private static void CheckInstance()
+        {
+            if (Instance != null)
+                return;
+
+            Instance = new GameObject(typeof(GravitySystem).Name).AddComponent<GravitySystem>();
+            //DontDestroyOnLoad(Instance.gameObject);
+        }
+
+        private void FixedUpdate()
+        {
+            //Apply forces / acceleration
+            foreach (var attractor in Masses)
+            {
+                if (attractor.Mass < double.Epsilon)
+                    continue;
+
+                foreach (var subject in Masses)
+                {
+                    if (attractor == subject)
+                        continue;
+
+                    var r = subject.Position - attractor.Position;
+                    var a = (float)(GravitationalConstant * attractor.Mass) / r.sqrMagnitude;
+                    subject.Velocity -= a * r.normalized * Time.fixedDeltaTime * (float)TimeSpeed;
+                }
+            }
+
+            //Apply velocity
+            foreach (var subject in Masses)
+                subject.Position += subject.Velocity * Time.fixedDeltaTime * (float)TimeSpeed;
+        }
+    }
+}
